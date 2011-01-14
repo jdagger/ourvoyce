f5d3f1ff3cbf5ca0ecd2347d23ddb2be8d343d10
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-01-14 14:31:08 -0500

    lookup table migration

---
+++ db/migrate/20110114184049_rebuild_lookup_tables.rb
@@ -0,0 +1,33 @@
+class RebuildLookupTables < ActiveRecord::Migration
+  def self.up
+    drop_table :chambers if table_exists? :chambers
+    create_table :chambers, :id => false do |t|
+      t.integer :id
+      t.string :name
+    end
+
+    drop_table :government_types if table_exists? :government_types
+    create_table :government_types, :id => false do |t|
+      t.integer :id
+      t.string :name
+    end
+
+    drop_table :media_types if table_exists? :media_types
+    create_table :media_types, :id => false  do |t|
+      t.integer :id
+      t.string :name
+      t.integer :level
+      t.integer :display_order
+    end
+
+    drop_table :states if table_exists? :states
+    create_table :states, :id => false do |t|
+      t.integer :id
+      t.string :abbreviation
+      t.string :name
+    end
+  end
+
+  def self.down
+  end
+end
