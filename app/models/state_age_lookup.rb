b5222c4e911411ed18093b8e853a0553e8b70dc3
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-01-20 17:15:49 -0500

    adding fiels

---
+++ app/models/state_age_lookup.rb
@@ -0,0 +1,2 @@
+class StateAgeLookup < ActiveRecord::Base
+end
---
+++ test/unit/national_age_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class NationalAgeTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
