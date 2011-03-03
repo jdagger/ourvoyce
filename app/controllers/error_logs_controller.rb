e0932568d5e80b240cb561bef5af8699c0e5751d
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-03-03 15:59:15 -0500

    Error log cleanup

--- app/controllers/error_logs_controller.rb
+++ app/controllers/error_logs_controller.rb
@@ -1,9 +1,25 @@
 class ErrorLogsController < AdminController
   require 'cgi'
   def index
-    @errors = ErrorLog.all
+    @error_count = ErrorLog.all.count
+
+    page_size = 10 
+    page = [params[:page].to_i, 1].max
+
+    @errors = ErrorLog.order("created_at desc").offset((page - 1) * page_size).limit(page_size)
+
+    @paging = PagingHelper::PagingData.new
+    @paging.total_pages = (@error_count.to_f / page_size).ceil
+    @paging.current_page = page
+    (1..@paging.total_pages).each do |count|
+      link = {:link_url => url_for(:controller => :error_logs, :action => :index, :page => count), :page => count}
+      @paging.links << link
+    end
+
+
     @errors.each do |error|
-      error.message = CGI.escapeHTML(CGI::unescape(error.message)).html_safe
+      error.request = CGI.escapeHTML(CGI::unescape(error.request)).html_safe
+      error.response = CGI.escapeHTML(CGI::unescape(error.response)).html_safe
     end
   end
 end
