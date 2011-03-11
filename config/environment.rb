2e49dd635e1c7c50a79d5772861de1a7e4be9a8a
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-03-11 17:34:45 -0500

    Setting up sendmail

--- config/environment.rb
+++ config/environment.rb
@@ -12,3 +12,4 @@ Rails.configuration.autoload_paths += %W(#{Rails.root.to_s}/lib)
 Rails.configuration.logos_domain = "https://s3.amazonaws.com/ourvoYce"
 Rails.configuration.host = "www.ourvoyce.com"
 Rails.configuration.default_page_size = 50
+ActionMailer::Base.delivery_method = :sendmail
