9e04eeb758f14132bdf4361f1c9cf388eb962545
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-03-11 17:59:00 -0500

    actionmailer bug fix

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,9 +1,10 @@
 class AccountMailer < ActionMailer::Base
   default :from => "accounts@ourvoyce.com"
+  default_url_options[:host] = "ourvoyce.com"
 
   def password_reset_email(user)
     #@user = user
-    @password_reset_url = edit_reset_password_url(:host => "ourvoyce.com", user.perishable_token)
+    @password_reset_url = edit_reset_password_url(user.perishable_token)
     mail(:to => user.email, :subject => 'Password Reset')
   end
 end
