d6caa887b95ea140c453616f54e4321ea8955c74
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-03-15 22:53:11 -0400

    Adding delayed job for mailer

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'http://rubygems.org'
 #gem 'rails', '3.0.3'
 gem 'rails'
 
+gem 'delayed_job'
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,9 +1,8 @@
 class AccountMailer < ActionMailer::Base
   default :from => "accounts@ourvoyce.com"
-  default_url_options[:host] = "ourvoyce.com"
+  default_url_options[:host] = "www.ourvoyce.com"
 
   def password_reset_email(user)
-    #@user = user
     @password_reset_url = edit_reset_password_url(user.perishable_token)
     mail(:to => user.email, :subject => 'Password Reset')
   end
