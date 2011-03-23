582ca638da5f8c0d0b268e624924b6f4e6c14792
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-03-23 11:46:21 -0400

    unicorn config cleanup

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -65,16 +65,16 @@ after_fork do |server, worker|
   # drop the workers to git:git
 
   begin
-    uid, gid = Process.euid, Process.egid
-    user, group = 'git', 'git'
-    target_uid = Etc.getpwnam(user).uid
-    target_gid = Etc.getgrnam(group).gid
-    worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid || gid != target_gid
-      Process.initgroups(user, target_gid)
-      Process::GID.change_privilege(target_gid)
-      Process::UID.change_privilege(target_uid)
-    end
+    #uid, gid = Process.euid, Process.egid
+    #user, group = 'git', 'git'
+    #target_uid = Etc.getpwnam(user).uid
+    #target_gid = Etc.getgrnam(group).gid
+    #worker.tmp.chown(target_uid, target_gid)
+    #if uid != target_uid || gid != target_gid
+      #Process.initgroups(user, target_gid)
+      #Process::GID.change_privilege(target_gid)
+      #Process::UID.change_privilege(target_uid)
+    #end
   rescue => e
     if RAILS_ENV == 'development'
       STDERR.puts "couldn't change user, oh well"
