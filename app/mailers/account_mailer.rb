aee1d2752575f87e8c68ee8ae06eb8a7b08f44fc
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-03-30 08:30:50 -0400

    Mailer bug fix

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -8,16 +8,20 @@ class AccountMailer < ActionMailer::Base
   end
 
   def verification_email(user)
-    subject "OurVoyce Email Verification"
-    recipients user.email
-    sent_on Time.now
-    body :verification_url => verification_url(user.perishable_token)
+    #subject "OurVoyce Email Verification"
+    #recipients user.email
+    #sent_on Time.now
+    #body :verification_url => verification_url(user.perishable_token)
+    @verification_url = verification_url(user.perishable_token)
+    mail(:to => user.email, :subject => 'OurVoyce Email Verification')
   end
 
   def forgot_username_email(user)
-    subject "OurVoyce Username"
-    recipients user.email
-    sent_on Time.now
-    body :username => user.login
+    #subject "OurVoyce Username"
+    #recipients user.email
+    #sent_on Time.now
+    #body :username => user.login
+    @username = user.login
+    ail(:to => user.email, :subject => 'OurVoyce Username')
   end
 end
