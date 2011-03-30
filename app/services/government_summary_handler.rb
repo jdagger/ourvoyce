d9eecd18d058b34a47fc83df189f121a28427bb2
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-03-30 15:54:15 -0400

    iphone media and government sspr

--- app/services/government_summary_handler.rb
+++ app/services/government_summary_handler.rb
@@ -1,13 +1,41 @@
 class GovernmentSummaryHandler < HandlerBase
 
+  attr_accessor :executive_ss
+  attr_accessor :executive_pr
+
+  attr_accessor :agency_ss
+  attr_accessor :agency_pr
+
+  attr_accessor :leg_ss
+  attr_accessor :leg_pr
+
+  def handle_request
+    super
+    gov_types = GovernmentType.find(:all, :order => :display_order)
+    gov_types.each do |gov|
+      case gov.id
+      when 1
+        self.agency_ss = gov.social_score
+        self.agency_pr = gov.participation_rate
+      when 2
+        self.executive_ss = gov.social_score
+        self.executive_pr = gov.participation_rate
+      when 3
+        self.leg_ss = gov.social_score
+        self.leg_pr = gov.participation_rate
+      end
+    end
+  end
+
 	def populate_result(result_hash)
 		result_hash["Body"] = {}
+		result_hash["Body"] = {"Status" => self.status }
 		body = result_hash["Body"]
-		body["AgencySocialScore"] = 10
-		body["AgencyParticipationRate"] = participation_rate_image(20)
-		body["ExecutiveSocialScore"] = 30
-		body["ExecutiveParticipationRate"] = participation_rate_image(40)
-		body["LegislativeSocialScore"] = 50
-		body["LegislativeParticipationRate"] = participation_rate_image(60)
+		body["AgencySocialScore"] = self.agency_ss
+		body["AgencyParticipationRate"] = participation_rate_image(self.agency_pr)
+		body["ExecutiveSocialScore"] = self.executive_ss
+		body["ExecutiveParticipationRate"] = participation_rate_image(self.executive_pr)
+		body["LegislativeSocialScore"] = self.leg_ss
+		body["LegislativeParticipationRate"] = participation_rate_image(self.leg_pr)
 	end
 end
--- app/services/media_summary_handler.rb
+++ app/services/media_summary_handler.rb
@@ -1,18 +1,45 @@
 class MediaSummaryHandler < HandlerBase
+  attr_accessor :newspaper_ss
+  attr_accessor :newspaper_pr
+  attr_accessor :magazine_ss
+  attr_accessor :magazine_pr
+  attr_accessor :tv_ss
+  attr_accessor :tv_pr
+  attr_accessor :radio_ss
+  attr_accessor :radio_pr
+
 	def handle_request
     super
+
+    media_types = MediaType.all
+    media_types.each do |type|
+     case type.id
+     when 1
+       self.magazine_ss = type.social_score
+       self.magazine_pr = type.participation_rate
+     when 2
+       self.newspaper_ss = type.social_score
+       self.newspaper_pr = type.participation_rate
+     when 3
+       self.radio_ss = type.social_score
+       self.radio_pr = type.participation_rate
+     when 4
+       self.tv_ss = type.social_score
+       self.tv_pr = type.participation_rate
+     end
+    end
 	end
 
 	def populate_result(result_hash)
 		result_hash["Body"] = {}
 		body = result_hash["Body"]
-		body["NewspaperSocialScore"] = 10
-		body["NewspaperParticipationRate"] = participation_rate_image(20)
-		body["MagazineSocialScore"] = 30
-		body["MagazineParticipationRate"] = participation_rate_image(40)
-		body["TVSocialScore"] = 50
-		body["TVParticipationRate"] = participation_rate_image(60)
-		body["RadioSocialScore"] = 70
-		body["RadioParticipationRate"] = participation_rate_image(80)
+		body["NewspaperSocialScore"] = self.newspaper_ss
+		body["NewspaperParticipationRate"] = participation_rate_image(self.newspaper_pr)
+		body["MagazineSocialScore"] = self.magazine_ss
+		body["MagazineParticipationRate"] = participation_rate_image(self.magazine_pr)
+		body["TVSocialScore"] = self.tv_ss
+		body["TVParticipationRate"] = participation_rate_image(self.tv_pr)
+		body["RadioSocialScore"] = self.radio_ss
+		body["RadioParticipationRate"] = participation_rate_image(self.radio_pr)
 	end
 end
