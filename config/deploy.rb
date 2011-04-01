25be0c7bae259551d56bde7aad8379a1a5e3bed2
Ryan Calvert <ryan.m.calvert@gmail.com>
2011-04-01 12:53:49 -0400

    Deploy cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,13 +23,14 @@ role :db,  "50.56.91.61", :primary => true # This is where Rails migrations will
 namespace :deploy do
   desc "Stopping server"
   task :stop do
+    find_and_execute_task("unicorn:stop")
     #find_and_execute_task("unicorn:stop")
     #find_and_execute_task("thinking_sphinx:stop")
   end
 
   desc "Starting server"
   task :start do
-    find_and_execute_task("unicorn:upgrade")
+    find_and_execute_task("unicorn:start")
 
     #find_and_execute_task("unicorn:start")
     #find_and_execute_task("thinking_sphinx:rebuild")
@@ -37,6 +38,7 @@ namespace :deploy do
 
   desc "Restarting server"
   task :restart do
+    find_and_execute_task("unicorn:upgrade")
     #find_and_execute_task("unicorn:restart")
     #find_and_execute_task("thinking_sphinx:rebuild")
   end
